d2cf60318501fe5e3bb98224fa8520abce22ac98
Marc Petitdemange <marc.petitdemange57@gmail.com>
2021-12-16 22:44:15 +0100

    Début des modifications du projet - Ajout du header + Routage + Ajout des premiers assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {PersonnelComponent} from "./personnel/personnel.component";
+import {GraphComponent} from "./graph/graph.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
+  {path:'accueil', component: AccueilComponent},
+  {path:'personnel', component: PersonnelComponent},
+  {path:'graph', component: GraphComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { AccueilComponent } from './accueil/accueil.component';
+import { PersonnelComponent } from './personnel/personnel.component';
+import { GraphComponent } from './graph/graph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    PersonnelComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
