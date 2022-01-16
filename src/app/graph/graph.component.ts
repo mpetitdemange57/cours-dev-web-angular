92540244eac5676140d66f940640eca61cd8ea15
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-16 14:52:17 +0100

    Correction des problèmes du graphique

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {EChartsOption} from "echarts";
 import {ListPersonnelService} from "../partage/service/list-personnel.service";
 import {ThemeOption} from "ngx-echarts";
@@ -8,9 +8,7 @@ import {ThemeOption} from "ngx-echarts";
   templateUrl: './graph.component.html',
   styleUrls: ['./graph.component.scss']
 })
-export class GraphComponent implements OnInit {
-
-  @ViewChild(ElementRef) chart!:ElementRef;
+export class GraphComponent {
 
   theme: ThemeOption = {
     itemStyle:{
@@ -42,26 +40,34 @@ export class GraphComponent implements OnInit {
 
 
   constructor(private listPersonnelService:ListPersonnelService) {
+  }
+
+
+  /**
+   * Fonction qui va chercher les données par un appel au server
+   * Une fois les données récupérées, on itère dessus pour effectuer un treaitement
+   * Une fois tout cela fini, on ajoute les nouvelles options au graphique (ce qui va l'actualiser)
+   * @param myChart l'instance du graphique
+   */
+  onChartInit(myChart: any) {
     let map: Map<string, any> = new Map();
     map.set("M", {value: 0, itemStyle: {color: '#8377f5'}});
     map.set("F", {value: 0, itemStyle: {color: '#c700b4'}});
     map.set("A", {value: 0, itemStyle: {color: '#56ff00'}});
     this.listPersonnelService.fetch().forEach((personnel) => {
-      personnel.forEach((employe:any) => {
+      personnel.forEach((employe: any) => {
         if (employe.sexe === "M") {
           map.get('M').value!++;
         } else if (employe.sexe === "F") {
           map.get('F').value!++;
-        } else{
+        } else {
           map.get('A').value!++;
         }
         let valeurs = Array.from(map.values());
-        this.chartOption.series  = [{type:"bar", color: ['blue','red','yellow'],data: valeurs}];
+        this.chartOption.series = [{type: "bar", color: ['blue', 'red', 'yellow'], data: valeurs}];
       });
+    }).then (() => {
+      myChart.setOption(this.chartOption);
     });
-  }
-
-  ngOnInit(): void {
-    this.chart.nativeElement.setOption(this.chartOption);
   }
 }
