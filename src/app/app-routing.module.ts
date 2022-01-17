caee41818aff5803760a7669754f4f28a59c2e1a
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-17 22:26:49 +0100

    Exercice 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {AccueilComponent} from "./accueil/accueil.component";
-import {ListPersonnelComponent} from "./list-personnel/list-personnel.component";
-import {GraphComponent} from "./graph/graph.component";
-import {ContactComponent} from "./contact/contact.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'accueil', pathMatch: 'full' },
-  {path:'accueil', component: AccueilComponent},
-  {path:'listPersonnel', component: ListPersonnelComponent},
-  {path:'graph', component: GraphComponent},
-  {path:'contact', component: ContactComponent}
-];
+const routes: Routes = [];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
