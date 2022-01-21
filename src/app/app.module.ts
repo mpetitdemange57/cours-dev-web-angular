7e8b894539d3b395260c4df792e425430652f97e
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-21 22:54:13 +0100

    12.0-Traduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatListModule} from "@angular/material/list";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -28,15 +28,6 @@ import { DrawerComponent } from './drawer/drawer.component';
 import { ListPersonnelComponent } from './list-personnel/list-personnel.component';
 import { GraphComponent } from './graph/graph.component';
 import { ContactComponent } from './contact/contact.component';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {TranslateHttpLoader} from "@ngx-translate/http-loader";
-import {LangueBoutonComponent} from "./langue-bouton/langue-bouton.component";
-import {MatMenuModule} from "@angular/material/menu";
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
-
 
 @NgModule({
   declarations: [
@@ -47,8 +38,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     DrawerComponent,
     ListPersonnelComponent,
     GraphComponent,
-    ContactComponent,
-    LangueBoutonComponent
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -73,18 +63,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts')
     }),
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-
     MatRadioModule,
     MatGridListModule,
-    MatMenuModule,
     // or import('./path-to-my-custom-echarts')
   ],
   providers: [],
