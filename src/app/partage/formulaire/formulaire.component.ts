daba384f932611605fd0ac0851305515f7be8403
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-31 21:28:14 +0100

    21.0-rf-solution

--- src/app/partage/formulaire/formulaire.component.ts
+++ src/app/partage/formulaire/formulaire.component.ts
@@ -1,4 +1,7 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {MatChipInputEvent} from "@angular/material/chips";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Person} from "../../model/Person";
 
 @Component({
@@ -7,19 +10,36 @@ import {Person} from "../../model/Person";
   styleUrls: ['./formulaire.component.scss']
 })
 export class FormulaireComponent implements OnInit {
-  @Input() employeModel: Person = {};
+  form: FormGroup;
+  @Input() employeModel: Person;
   @ViewChild("fileInput") fileInput!: ElementRef;
 
   @Output('cancel') cancelEvent$: EventEmitter<any>;
   @Output('submit') submitEvent$: EventEmitter<any>;
 
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+
   constructor() {
     this.submitEvent$ = new EventEmitter();
     this.cancelEvent$ = new EventEmitter();
+    this.form = FormulaireComponent.buildForm();
+    this.employeModel = {
+      titres: []
+    };
   }
 
   ngOnInit() {
-    //VIDE
+    debugger;
+    this.form.patchValue({
+      id: this.employeModel.id,
+      nom: this.employeModel.nom,
+      prenom: this.employeModel.prenom,
+      email: this.employeModel.email,
+      titres: this.employeModel.titres || [],
+      sexe: this.employeModel.sexe,
+      photo: this.employeModel.photo,
+      telephone: this.employeModel.telephone
+    });
   }
 
   cancel() {
@@ -27,7 +47,55 @@ export class FormulaireComponent implements OnInit {
   }
 
   submit(employe: Person) { //Formulaire
-    debugger;
+    employe.photo = this.employeModel.photo;
     this.submitEvent$.emit(employe);
   }
+
+
+  addChipset(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.employeModel.titres!.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
+  removeChipset(titre: any): void {
+    const index = this.employeModel.titres!.indexOf(titre);
+    this.employeModel.titres!.splice(index, 1);
+  }
+
+  onFileSelected(event:Event | null) {
+    const files = (<HTMLInputElement>event?.currentTarget).files;
+    const file:File | null = files!.item(0);
+
+    if (file) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (_event) => {
+          this.employeModel.photo = reader.result;
+        }
+    }
+  }
+
+  /**
+   * Fonction pour construire notre formulaire
+   * @returns {FormGroup}
+   *
+   * @private
+   */
+  private static buildForm(): FormGroup {
+    return new FormGroup({
+      id: new FormControl(''),
+      prenom: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+      nom: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
+      email: new FormControl('', Validators.required),
+      titres: new FormControl(''),
+      sexe: new FormControl(''),
+      telephone: new FormControl('', Validators.compose([Validators.required, Validators.pattern('\\d{10}')])),
+    });
+  }
+
+
+
 }
