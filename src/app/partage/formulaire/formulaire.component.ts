0af7e45e053fb2c90144039a4a4e693e7e659c0c
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-01-31 21:20:19 +0100

    20.0-Tdf-exo

--- src/app/partage/formulaire/formulaire.component.ts
+++ src/app/partage/formulaire/formulaire.component.ts
@@ -1,5 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {Person} from "../../model/Person";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'formulaire',
@@ -7,27 +6,12 @@ import {Person} from "../../model/Person";
   styleUrls: ['./formulaire.component.scss']
 })
 export class FormulaireComponent implements OnInit {
-  @Input() employeModel: Person = {};
-  @ViewChild("fileInput") fileInput!: ElementRef;
-
-  @Output('cancel') cancelEvent$: EventEmitter<any>;
-  @Output('submit') submitEvent$: EventEmitter<any>;
 
   constructor() {
-    this.submitEvent$ = new EventEmitter();
-    this.cancelEvent$ = new EventEmitter();
+    //COMMENTAIRE
   }
 
   ngOnInit() {
     //VIDE
-  }
-
-  cancel() {
-    this.cancelEvent$.emit();
-  }
-
-  submit(employe: Person) { //Formulaire
-    debugger;
-    this.submitEvent$.emit(employe);
   }
 }
