d098a6d09ecedb7fefc41ba42fbed5091afd354b
Marc Petitdemange <marc.petitdemange57@gmail.com>
2022-12-30 18:38:15 +0100

    Passage en Angular 15 + Fix de certains problèmes concernant Angular Material. A faire : Modifier la barre de recherche

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import languages from "./languages.json";
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  public languages: any = languages;
 
   constructor() { }
 
